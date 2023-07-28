94499836898c5564ead0e8642d119e443e81d34b
Conor O'Brien <32160798+Con-OBrien@users.noreply.github.com>
2023-07-28 19:14:06 +0100

    HackerRank SQL Queries

---
+++ employeeNames.sql
@@ -0,0 +1,4 @@
+-- Write a query that prints a list of employee names from the Employee table in alphabetical order
+select name
+from Employee
+order by name
---
+++ weatherObservation1.sql
@@ -0,0 +1,5 @@
+-- Query a list of City and State from the Station table
+-- https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+
+SELECT CITY, STATE
+FROM STATION;
